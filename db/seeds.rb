adae55f763ff5f49c48bbcef13085ae2a04ed7aa
eiji <holy.edge.nuxaxtuxtu@gmail.com>
2016-08-24 20:52:33 +0900

    add seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+4.times do |n|
+  email = "example#{sprintf("%003d", n+1)}@example.com"
+  password = "12345678"
+  user = User.new(email: email,
+               password: password,
+               password_confirmation: password,
+               name: Faker::Name.name,
+               uid: SecureRandom.uuid)
+  user.skip_confirmation!
+  user.save
+
+  3.times do |m|
+    Topic.create!(content: Faker::Lorem.sentence,
+                 user_id: user.id,
+                 created_at: Time.now)
+  end
+end
