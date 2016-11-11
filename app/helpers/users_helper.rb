04189347b11b1a9fe5253536bb50a908ff8cc7f1
eiji <holy.edge.nuxaxtuxtu@gmail.com>
2016-11-11 16:00:06 +0900

    自分のtopicが見えないのを修正

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,6 +2,10 @@ module UsersHelper
   def isfriend?(user)
     toarray = []
     toarray << user
-    current_user.friend == toarray
+    if current_user.friend == toarray
+      current_user.friend == toarray
+    elsif current_user == user
+      current_user == user
+    end
   end
 end
